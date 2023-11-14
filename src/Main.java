0c44a84398580cfcb352bade9878fffece7dd825
kv45 <kshitijverma852@gmail.com>
2023-11-14 14:49:39 +0000

    Get SelectionSort to work

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] nums = {1, 4, 6, 7, 8, 43};
+        System.out.println(Arrays.toString(SelectionSort.sort(nums)));
+    }
+}
---
+++ src/SelectionSort.java
@@ -0,0 +1,14 @@
+public class SelectionSort {
+    public static int[] sort(int[] nums) {
+        for (int x = 0; x < nums.length; x++) {
+            for (int y = x; y < nums.length; y++) {
+                if (nums[x] > nums[y]) {
+                    int temp = nums[x];
+                    nums[x] = nums[y];
+                    nums[y] = temp;
+                }
+            }
+        }
+        return nums;
+    }
+}
